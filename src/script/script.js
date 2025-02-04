10fcf4cdc6750912318aac55754acfdccdbfe064
Wilson Maximo <wilson-max@hotmail.com>
2025-02-04 17:07:11 -0300

    Estrutura corrigida

--- src/script/script.js
+++ src/script/script.js
@@ -8,15 +8,14 @@ const encadernacao = [...document.getElementsByName('encadernacao')];
 const frenteEVerso = [...document.getElementsByName('lado')];
 const cor = [...document.getElementsByName('cor')];
 const avisoValorMaximo = document.getElementById('avisoValorMaximo');
-const quantidadeMaxima = 500;
-const quantidadeMinima = 1;
 
 //Funções para cálculos e formatação de valores
 const calcularPrecoImpressao = (qtd) => {
   const precos = [
+    { limite: 1000, preco: 0.25 },
     { limite: 500, preco: 0.40 },
     { limite: 150, preco: 0.45 },
-    { limite: 50, preco: 0.50 },
+    { limite: 50, preco: 0.50 },  
   ];
   const faixa = precos.find(({ limite }) => qtd >= limite);
   return faixa ? faixa.preco : 1;
@@ -40,10 +39,10 @@ const processarNumero = (numero) => numero % 2 === 0 ? numero / 2 : (numero - 1)
 const calcularValorTotal = (paginas, preco, encadernacao) =>
   paginas * preco + encadernacao;
 
-//Funções para atualizar valores no HTML
 const seEFrente = (tipo, paginas, folhas) =>
   tipo === 'frenteEVerso' ? folhas : paginas;
 
+//Funções para atualizar valores no HTML
 const obterValoresAtuais = () => {
   const qtdDePaginas = parseInt(quantidadeDePaginas?.value) || 0;
   const qtdDeFolhas = processarNumero(qtdDePaginas);
@@ -75,13 +74,19 @@ const atualizarValores = () => {
   //encadernação
   const qtdDeFolhasOuPaginas = seEFrente(tipoFrenteEVerso, qtdDePaginas, qtdDeFolhas);
 
-  const precoEncadernacao =
-  tipoEncadernacao === 'comEncadernacao' ? calcularPrecoEncadernacao(qtdDeFolhasOuPaginas) : 0;
+  var precoEncadernacao = 0;
 
-  tipoEncadernacao === 'comEncadernacao' ?
+  if (tipoEncadernacao === 'comEncadernacao') {
+    quantidadeDePaginas.max = 500;
+    quantidadeDePaginas.value > 500 ? quantidadeDePaginas.value = 500 : null;
+    precoEncadernacao = calcularPrecoEncadernacao(qtdDeFolhasOuPaginas);
     valorEncadernacao.textContent =
-      `Valor da Encadernação: R$${formatarValor(precoEncadernacao)}` :
+      `Valor da Encadernação: R$${formatarValor(precoEncadernacao)}`;
+  } else {
+    quantidadeDePaginas.max = 1000;
+    precoEncadernacao = 0;
     valorEncadernacao.textContent = '';
+  }
 
   //impressão
   const precoImpressao = calcularPrecoImpressao(qtdDePaginas);
@@ -91,21 +96,6 @@ const atualizarValores = () => {
   const novoValor = calcularValorTotal(qtdDePaginas, precoImpressao, precoEncadernacao);
 
   valorTotal.textContent = `Valor Total: R$${formatarValor(novoValor)}`;
-};
-
-const setarEntrada = () => {
-  const { qtdDeFolhas } = obterValoresAtuais();
-
-  if (qtdDeFolhas > quantidadeMaxima) {
-    quantidadeDePaginas.value = quantidadeMaxima;
-    avisoValorMaximo.style.display = 'inline';
-  } else {
-    avisoValorMaximo.style.display = 'none';
-  }
-
-  if (qtdDeFolhas < quantidadeMinima) {
-    quantidadeDePaginas.value = quantidadeMinima;
-  }
 };
 
 const buscarTexto = (id) => {
@@ -119,8 +109,8 @@ const submitFormulario = (event) => {
   const idFrenteEVerso = buscarTexto(frenteEVerso.find(e => e.checked).id);
   const idColorido = buscarTexto(cor.find(e => e.checked).id);
   const nome = document.getElementById('inputNome').value;
-  const numero = document.getElementById('inputTelefone').value;
-  const email = document.getElementById('inputEmail')?.value;
+  // const numero = document.getElementById('inputTelefone').value;
+  //const email = document.getElementById('inputEmail')?.value;
 
   alert(`Olá ${nome}, você solicitou um orçamento para ${quantidadeDePaginas.value} páginas, ${idEncadernacao}, ${idFrenteEVerso} e ${idColorido}.
     
@@ -130,6 +120,33 @@ const submitFormulario = (event) => {
   formulario.reset();
 };
 
+const setarEntrada = () => {
+  const { qtdDeFolhas, tipoEncadernacao } = obterValoresAtuais();
+  const quantidadeMinima = 1;
+  const texto = buscarTexto(tipoEncadernacao).toLowerCase();
+  let quantidadeMaxima = 500;
+
+
+  if (tipoEncadernacao === 'comEncadernacao') {
+    quantidadeMaxima = 500;
+    quantidadeDePaginas.max = 500;
+  } else {
+    quantidadeMaxima = 1000;
+    quantidadeDePaginas.max = 1000;
+  }
+
+  if (qtdDeFolhas > quantidadeMaxima) {
+    quantidadeDePaginas.value = quantidadeMaxima;
+    avisoValorMaximo.textContent = `O valor máximo ${texto} é de ${quantidadeMaxima} páginas.`;
+    avisoValorMaximo.style.display = 'inline';
+  } else {
+    avisoValorMaximo.style.display = 'none';
+  }
+
+  if (qtdDeFolhas < quantidadeMinima) {
+    quantidadeDePaginas.value = quantidadeMinima;
+  }
+};
 
 //Eventos
 document.addEventListener('DOMContentLoaded', () => {
@@ -151,5 +168,6 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   formulario.addEventListener('submit', (event) => submitFormulario(event));
+  formulario.addEventListener('reset', atualizarValores);
 
 });
